93fefdde81fa1dee40be2550c06d400f681d03c7
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2021-03-18 11:51:17 +0530

    My First Commit

---
+++ TodoList/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RootfilesConfig(AppConfig):
+    name = 'TodoList'
---
+++ rootFiles/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rootFiles project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rootFiles.settings')
+
+application = get_wsgi_application()
